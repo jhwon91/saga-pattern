053ae9c2f2ffdeb987a7f5d7a0fb06e2b314f617
won <jwon3991.9191@gmail.com>
2026-03-12 21:07:36 +0900

    Notification Service 도메인 및 설정

--- service_3/src/main/java/com/saga/Main.java
+++
@@ -1,7 +0,0 @@
-package com.saga;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
--- service_3/src/main/java/com/saga/domain/Notification.java
+++ service_3/src/main/java/com/saga/domain/Notification.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Builder;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
@@ -35,4 +36,13 @@ public class Notification {
     @Column(name = "created_at")
     private LocalDateTime createdAt = LocalDateTime.now();
 
+    @Builder
+    public Notification(String notificationId, String userId, String sagaId, String notificationType, String message, String status) {
+        this.notificationId = notificationId;
+        this.userId = userId;
+        this.sagaId = sagaId;
+        this.notificationType = notificationType;
+        this.message = message;
+        this.status = status;
+    }
 }
